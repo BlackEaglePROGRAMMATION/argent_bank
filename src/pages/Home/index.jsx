923d2cff39f077ab1fd521a3b7ccd1f7377de6b1
Corentin Beaudet <corentin.beaudet14@gmail.com>
2023-10-31 13:23:47 +0100

    add content for home page, desktop version

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,30 @@
+import Feature from '../../components/Feature';
+import features from './../../datas/features';
+
 import './style.css'
 
 function Home() {
     return (
         <div className='home'>
-            
+            <div className="banner">
+                <div className="container">
+                    <h2>
+                        No fees.<br />
+                        No minimum deposit.<br />
+                        High interest rates.
+                    </h2>
+                    <p>Open a savings account with Argent Bank today!</p>
+                </div>
+            </div>
+
+            <section className="sect-features">
+                {features.map((feature, idx) =>
+                    <Feature
+                        key={idx}
+                        feature={feature}
+                    />
+                )}
+            </section>
         </div>
     )
 }
